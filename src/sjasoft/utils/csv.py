05074a3f905e6b41124ed0e34b3e67ef6d0eb840
Samantha Atkins <samantha@sjasoft.com>
2025-10-16 17:12:00 -0400

    some basic cleanup

--- src/sjasoft/utils/csv.py
+++ src/sjasoft/utils/csv.py
@@ -1,16 +1,16 @@
 import csv, io
-from sjasoft.utils import utils
+from sjasoft.utils.string import bytes_to_string
 
 
 def homogenize_fields(data):
-    '''
+    """
     Ensure values for all fields have consistent data by substituting defaults
     in case of missing value. In order words produce list of dicts that have
     the same keys and default values for missing keys but some but not all rows
     have.
     :param data: list of dictionaries
     :return: modifies data in place as necessary
-    '''
+    """
 
     def default_for_type(a_type):
         if issubclass(a_type, int):
@@ -18,9 +18,9 @@ def homogenize_fields(data):
         elif issubclass(a_type, float):
             return 0.0
         elif issubclass(a_type, str):
-            return ''
+            return ""
         else:
-            raise Exception('unhandled missing type %s' % a_type)
+            raise Exception("unhandled missing type %s" % a_type)
 
     key_type = {}
     for item in data:
@@ -35,13 +35,13 @@ def homogenize_fields(data):
 
 
 def dict_to_csv(dict_list, first_keys=None, postprocess_fn=None):
-    '''
+    """
     convert a list of dicts with same keys to a CSV in a buffer
     :param dict_list: list of dicts
     :param first_keys: keys that should be in first columns if we require it
     :param postprocess_fn: optional transformation te perform on each dict before generating csv output
     :return: io.StringIO() csv outputp
-    '''
+    """
     if isinstance(dict_list, dict):
         dict_list = [dict_list]
     if postprocess_fn:
@@ -50,8 +50,11 @@ def dict_to_csv(dict_list, first_keys=None, postprocess_fn=None):
     csv_columns = list(dict_list[0].keys())
     if first_keys:  # then we may have to reorder
         if not all([((k in csv_columns) for k in first_keys)]):
-            raise Exception('report configuration error, sort keys %s not in columns %s' % (first_keys, csv_columns))
-        if csv_columns[:len(first_keys)] != first_keys:  # then reorder
+            raise Exception(
+                "report configuration error, sort keys %s not in columns %s"
+                % (first_keys, csv_columns)
+            )
+        if csv_columns[: len(first_keys)] != first_keys:  # then reorder
             rest = [k for k in csv_columns if k not in first_keys]
             csv_columns = list(first_keys) + rest
 
@@ -67,12 +70,12 @@ def dict_to_csv(dict_list, first_keys=None, postprocess_fn=None):
 
 
 def csv_to_dicts(raw_contents):
-    as_str = utils.bytesToString(raw_contents)
+    as_str = bytes_to_string(raw_contents)
     reader = csv.DictReader(as_str.split())
     return [dict(r) for r in reader]
 
 
 def csv_to_arrays(raw_contents):
-    as_str = utils.bytesToString(raw_contents)
+    as_str = bytes_to_string(raw_contents)
     reader = csv.reader(as_str.split())
     return list(reader)
--- src/sjasoft/utils/tools.py
+++ src/sjasoft/utils/tools.py
@@ -1,124 +1,152 @@
 from collections import defaultdict
 import random, re, os
-import validators
 import subprocess as sub
 from functools import reduce
+import asyncio
+
 
 def pass_fail(items, test):
-  passed = [], failed = []
-  for item in items:
-    ctx = passed if test(item) else failed
-    ctx.append(item)
-  return passed, failed
+    passed = [], failed = []
+    for item in items:
+        ctx = passed if test(item) else failed
+        ctx.append(item)
+    return passed, failed
+
 
 def to_list(aDict):
-  '''
-  Convert nested dict with list and dict values to nested list.  Originally used as alternate
-  parsing of a UOP query dict representation. 
-  '''
-  raw = list(aDict.items())
-  res = []
-  for k,v in raw:
-    res.append(k)
-    if isinstance(v, list):
-      res.append([to_list(item) for item in v])
-    elif isinstance(v, dict):
-      res.append(to_list(v))
-    else:
-      res.append(v)
-  return res
+    """
+    Convert nested dict with list and dict values to nested list.  Originally used as alternate
+    parsing of a UOP query dict representation.
+    """
+    raw = list(aDict.items())
+    res = []
+    for k, v in raw:
+        res.append(k)
+        if isinstance(v, list):
+            res.append([to_list(item) for item in v])
+        elif isinstance(v, dict):
+            res.append(to_list(v))
+        else:
+            res.append(v)
+    return res
+
 
 class ByNameId:
-  '''
-  Provides by name and by id maps for dict like items that have both 'name' and '_id' fields
-  '''
-  def __init__(self, uses_name=True):
-    self._id_map = {}
-    self._name_map = {}
-    self._uses_name = uses_name
+    """
+    Provides by name and by id maps for dict like items that have both 'name' and '_id' fields
+    """
 
-  def id_map(self):
-    return dict(self._id_map)
+    def __init__(self, uses_name=True):
+        self._id_map = {}
+        self._name_map = {}
+        self._uses_name = uses_name
 
-  def name_map(self):
-    return dict(self._name_map)
+    def id_map(self):
+        return dict(self._id_map)
 
-  def values(self):
-    return self._id_map.values()
+    def name_map(self):
+        return dict(self._name_map)
 
-  def add_item(self, item):
-    self._id_map[item['_id']] = item
-    if self._uses_name:
-      self._name_map[item['name']] = item
+    def values(self):
+        return self._id_map.values()
 
-  def with_id(self, an_id):
-    return self._id_map.get(an_id)
+    def add_item(self, item):
+        self._id_map[item["_id"]] = item
+        if self._uses_name:
+            self._name_map[item["name"]] = item
 
-  def with_name(self, name):
-    return self._name_map.get(name)
+    def with_id(self, an_id):
+        return self._id_map.get(an_id)
 
-  def random_instance(self):
-    vals = list(self._id_map.values())
-    index = random.randint(0, len(vals)-1)
-    return vals[index] if vals else None
+    def with_name(self, name):
+        return self._name_map.get(name)
+
+    def random_instance(self):
+        vals = list(self._id_map.values())
+        index = random.randint(0, len(vals) - 1)
+        return vals[index] if vals else None
 
 
 def as_list(fn, *args, **kwargs):
-  """because python3 made way two many things generators and/or special objects"""
-  return list(fn(*args, **kwargs))
+    """because python3 made way two many things generators and/ or special objects"""
+    return list(fn(*args, **kwargs))
+
 
 def lmap(fn, *iterables):
-  return as_list(map, fn, *iterables)
+    return as_list(map, fn, *iterables)
 
 
 def set_and(fn, values):
-  res = fn(values[0])
-  for v in values[1:]:
-    res_v = fn(v)
-    res = res & res_v
-    if not res:
-      break
-  return res
+    res = fn(values[0])
+    for v in values[1:]:
+        res_v = fn(v)
+        res = res & res_v
+        if not res:
+            break
+    return res
+
+
+async def a_set_and(fn, values):
+    current = await fn(values[0])
+    if not current:
+        return current
+    for v in values[1:]:
+        current_v = await fn(v)
+        current = current & current_v
+        if not current:
+            break
+    return current
+
 
 def set_or(fn, values):
-  return reduce(lambda a, b: a | b, map(fn, values), set())
+    return reduce(lambda a, b: a | b, map(fn, values), set())
+
+
+async def a_set_or(fun, items):
+    cos = [fun(item) for item in items]
+    sets = await asyncio.gather(cos)
+    return reduce(lambda a, b: a | b, sets, set())
 
 
 def match_fields(pat, aString, *fields):
-
-
     match = re.search(pat, aString)
     data = match.groupdict() if match else None
     return [data.get(f, None) for f in fields] if data else [None for _ in fields]
 
+
 def sub_pipes(*pipes):
     return {p: sub.PIPE for p in pipes}
-    
-standard_pipes = sub_pipes('stdin', 'stdout', 'stderr')
+
+
+standard_pipes = sub_pipes("stdin", "stdout", "stderr")
+
 
 def bytesToString(val):
-  return val.decode('utf-8') if isinstance(val, bytes) else val
+    return val.decode("utf-8") if isinstance(val, bytes) else val
+
 
 def without_output(cmd):
-  with open(os.devnull, 'a') as out:
-    sub.Popen(cmd, shell=True, stdout=out, stderr=out)
+    with open(os.devnull, "a") as out:
+        sub.Popen(cmd, shell=True, stdout=out, stderr=out)
+
 
 def with_output_to(path, cmd):
-  with open(path, 'a') as out:
-    sub.Popen(cmd, shell=True, stdout=out, stderr=out)
+    with open(path, "a") as out:
+        sub.Popen(cmd, shell=True, stdout=out, stderr=out)
+
 
 def command_output(command):
-  standard_pipes = dict(
-    stdout=sub.PIPE, stdin=sub.PIPE, stderr=sub.PIPE)
-  get_output = lambda stuff: [l.strip() for l in stuff.split('\n') if l]
-  p = sub.Popen(command, shell=True, **standard_pipes)
-  out, err = p.communicate()
-  
-  res =  get_output(bytesToString(out)) or get_output(bytesToString(err))
-  return res[0] if (len(res) == 1) else res
+    standard_pipes = dict(stdout=sub.PIPE, stdin=sub.PIPE, stderr=sub.PIPE)
+    get_output = lambda stuff: [l.strip() for l in stuff.split("\n") if l]
+    p = sub.Popen(command, shell=True, **standard_pipes)
+    out, err = p.communicate()
+
+    res = get_output(bytesToString(out)) or get_output(bytesToString(err))
+    return res[0] if (len(res) == 1) else res
+
 
 def writable_files_in(path):
-    data = command_output('ls -lR %s' % path)
+    data = command_output("ls -lR %s" % path)
     switch_path = False
     res = []
     for d in data:
@@ -128,182 +156,182 @@ def writable_files_in(path):
             continue
         if switch_path:
             path = ds[0]
-            switch_path=False
+            switch_path = False
             continue
-        if d.startswith('total'):
+        if d.startswith("total"):
             continue
         base = ds[-1]
-        #print 'base:', base
+        # print 'base:', base
         res.append(os.path.join(path, ds[-1]))
     return res
 
 
 def gensym(object):
-  """generate and return a symbol (att ribute name typically) unique to the object's attributes and method names"""
-  trial = 'hash_%X' % random.getrandbits(16)
-  while hasattr(object, trial):
-    trial = 'hash_%X' % random.getrandbits(16)
-  return trial
+    trial = "hash_%X" % random.getrandbits(16)  # noqa: F821
+    while hasattr(object, trial):
+        trial = "hash_%X" % random.getrandbits(16)
+    return trial
 
 
 def force_unicode(x):
-  return x.decode('utf-8') if isinstance(x, str) else x
+    return x.decode("utf-8") if isinstance(x, str) else x
 
 
 identity_function = lambda x: x
 
 
 def n_defaultdict(n, a_type):
-  maker = lambda t: lambda: defaultdict(t)
-  for _ in range(n):
-    a_type = maker(a_type)
-  return a_type()
+    maker = lambda t: lambda: defaultdict(t)
+    for _ in range(n):
+        a_type = maker(a_type)
+    return a_type()
 
 
-def tree_order(hierarchy, sequence, h_extractor=identity_function, s_extractor=identity_function):
-  """
-  A generator returning items of sequence in the order they appear traversing the
-  hierarchy (depth-first pre-order).
-  """
-  data = dict([(s_extractor(x), x) for x in sequence])
-  for value in hierarchy.pre_order(h_extractor):
-    if value in data:
-      yield data[value]
+def tree_order(
+    hierarchy, sequence, h_extractor=identity_function, s_extractor=identity_function
+):
+    """
+    A generator returning items of sequence in the order they appear traversing the
+    hierarchy (depth-first pre-order).
+    """
+    data = dict([(s_extractor(x), x) for x in sequence])
+    for value in hierarchy.pre_order(h_extractor):
+        if value in data:
+            yield data[value]
 
 
 def not_empty(seq):
-  """returns None if sequence is empty else a generator on the sequence. Good for checking generator
-  contains anything at all. Of course you would hang waiting on a generator that waits.."""
+    """returns None if sequence is empty else a generator on the sequence. Good for checking generator
+    contains anything at all. Of course you would hang waiting on a generator that waits..
+    """
 
-  def list_gen(first, rest):
-    yield first
-    for x in rest:
-      yield x
+    def list_gen(first, rest):
+        yield first
+        for x in rest:
+            yield x
 
 
 def pruning_tree_collect(root, children_function, test_function, result_function=None):
-  """
-  returns the nodes closest to the root nodes of the tree that satisfy the test_function.
-  The value returned for a satisfying node is determined by the result_function which
-  defaults to the node itself.
-  """
-  if result_function is None: 
-    result_function = identity_function
-  results = []
+    """
+    returns the nodes closest to the root nodes of the tree that satisfy the test_function.
+    The value returned for a satisfying node is determined by the result_function which
+    defaults to the node itself.
+    """
+    if result_function is None:
+        result_function = identity_function
+    results = []
 
-  def do_node(node):
-    # print 'evaluating node %s' % node
-    if test_function(node):
-      # print 'node %s satisfied test' % node
-      results.append(result_function(node))
-    else:
-      # print 'node %s did not satisfy test so examining children %s' % (node, children_function(node))
-      for child in children_function(node):
-        do_node(child)
+    def do_node(node):
+        # print 'evaluating node %s' % node
+        if test_function(node):
+            # print 'node %s satisfied test' % node
+            results.append(result_function(node))
+        else:
+            # print 'node %s did not satisfy test so examining children %s' % (node, children_function(node))
+            for child in children_function(node):
+                do_node(child)
 
-  do_node(root)
-  return results
+    do_node(root)
+    return results
 
 
 def all_satisfy(func, sequence):
-  """
-  Returns (True, None) if all elements satisfy func else (False, element) of the first element that
-  does not satisfy func
-  """
-  for s in sequence:
-    if not func(s):
-      return False, s
-  return True, None
+    """
+    Returns (True, None) if all elements satisfy func else (False, element) of the first element that
+    does not satisfy func
+    """
+    for s in sequence:
+        if not func(s):
+            return False, s
+    return True, None
 
 
 def one_satisfies(func, sequence):
-  "the any builtin unfortunately does not return the element that satisfied the func"
-  for s in sequence:
-    if func(s):
-      return True, s
-  return False, None
+    "the any builtin unfortunately does not return the element that satisfied the func"
+    for s in sequence:
+        if func(s):
+            return True, s
+    return False, None
 
 
-def identity(x): return x
-
-
+def identity(x):
+    return x
 
 
 def unique(sequence, hash_converter=None):
-  """returns the unique elements in the sequence. Note that the raw form
-  would only work if elements in the sequence are all hashable.  Passing a
-  hash_converter that can map elements that are the "same" to the
-  same hashable gets around this issue for many cases"""
+    """returns the unique elements in the sequence. Note that the raw form
+    would only work if elements in the sequence are all hashable.  Passing a
+    hash_converter that can map elements that are the "same" to the
+    same hashable gets around this issue for many cases"""
 
-  seen = set()
-  convert = hash_converter is not None
-  for s in sequence:
-    hashable = hash_converter(s) if convert else s
-    if not hashable in seen:
-      seen.add(hashable)
-      yield s
-  del seen
+    seen = set()
+    convert = hash_converter is not None
+    for s in sequence:
+        hashable = hash_converter(s) if convert else s
+        if not hashable in seen:
+            seen.add(hashable)
+            yield s
+    del seen
 
 
 def hexord2str(ho):
-  h = [ho[i:i + 2] for i in range(0, len(ho), 2)]
-  ints = [int(x, 16) for x in h]
-  return ''.join([chr(i) for i in ints])
+    h = [ho[i : i + 2] for i in range(0, len(ho), 2)]
+    ints = [int(x, 16) for x in h]
+    return "".join([chr(i) for i in ints])
 
 
 def str2hexord(s):
-  def hex2(n):
-    res = hex(n)[2:]
-    return res if len(res) == 2 else '0' + res
+    def hex2(n):
+        res = hex(n)[2:]
+        return res if len(res) == 2 else "0" + res
 
-  return ''.join([hex2(ord(x)) for x in s])
+    return "".join([hex2(ord(x)) for x in s])
 
 
 def encrypt(key, val):
-  def ith(x, i):
-    return ord(x[i]) if i < len(x) else 0
+    def ith(x, i):
+        return ord(x[i]) if i < len(x) else 0
 
-  def mod_len(x, i):
-    return ord(x[i % len(x)])
+    def mod_len(x, i):
+        return ord(x[i % len(x)])
 
-  l = max(len(key), len(val))
-  k = [mod_len(key, i) for i in range(l)]
-  v = [ith(val, i) for i in range(l)]
-  tangled = [chr(k[i] ^ v[i]) for i in range(l)]
-  return ''.join(tangled)
+    l = max(len(key), len(val))
+    k = [mod_len(key, i) for i in range(l)]
+    v = [ith(val, i) for i in range(l)]
+    tangled = [chr(k[i] ^ v[i]) for i in range(l)]
+    return "".join(tangled)
 
 
 def decrypt(key, code):
-  l = max(len(key), len(code))
+    l = max(len(key), len(code))
 
-  def mod_len(x, i):
-    return ord(x[i % len(x)])
+    def mod_len(x, i):
+        return ord(x[i % len(x)])
 
-  k = [mod_len(key, i) for i in range(l)]
-  c = [ord(x) for x in code]
-  ut = [chr(k[i] ^ c[i]) for i in range(l) if k[i] != c[i]]
-  return ''.join(ut)
+    k = [mod_len(key, i) for i in range(l)]
+    c = [ord(x) for x in code]
+    ut = [chr(k[i] ^ c[i]) for i in range(l) if k[i] != c[i]]
+    return "".join(ut)
 
 
 def plain2cipher(key, plain):
-  return str2hexord(encrypt(key, plain))
+    return str2hexord(encrypt(key, plain))
 
 
 def cipher2plain(key, cipher):
-  return decrypt(key, hexord2str(cipher))
+    return decrypt(key, hexord2str(cipher))
+
 
 def splitter(lst):
-  sz = len(lst)
-  if sz == 0:
-    return [], []
-  if sz == 1:
-    return lst, []
-  if sz == 2:
-    return lst[:-1], lst[-1:]
-  return lst[:sz / 2], lst[sz / 2:]
+    sz = len(lst)
+    if sz == 0:
+        return [], []
+    if sz == 1:
+        return lst, []
+    if sz == 2:
+        return lst[:-1], lst[-1:]
+    return lst[: sz / 2], lst[sz / 2 :]
 
 
 def random_pick(lst):
-  return lst[random.randint(0, len(lst) - 1)]
-
-
+    return lst[random.randint(0, len(lst) - 1)]
